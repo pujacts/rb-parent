76f696ec7ebe8e6ada7adcf63c828fce5d1493f8
pujacts <pujapandey73020@gmail.com>
2024-04-20 09:21:04 +0530

    created parent to manage pom

---
+++ src/main/java/com/rak/studentparent/StudentParentApplication.java
@@ -0,0 +1,13 @@
+package com.rak.studentparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rak/studentparent/StudentParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rak.studentparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
